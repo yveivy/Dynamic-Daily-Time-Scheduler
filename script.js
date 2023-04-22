b33d78ee38c1ab1f6195deb27fe5c4eae55d6576
Valais Girl <Yevette@Hal-71.local>
2023-04-21 22:32:33 -0500

    set local storage function

--- script.js
+++ script.js
@@ -1,69 +1,63 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
 $(document).ready(function() {
+ //
+  // TODO: Add code to display the current date in the header of the page.
+ 
+  // Display current time in header
   const currentTimeElement = $('#current-time');
-
   setInterval(() => {
-    const now = dayjs();
-    const nowFormatted = now.format('h:mm:ss A');
+    var now = dayjs();
+    const nowFormatted = now.format('dddd, MMMM D, YYYY hh:mm:ss A');
     currentTimeElement.text(nowFormatted);
-    updateBlockColors(now);
-    // highlightCurrentScheduleTime(now);
   }, 1000);
 
-  function updateBlockColors(now) {
-    $('.row time-block').each(function(){
-    const start = dayjs($(this).data('start'), 'h:mm:ss A');
-    const end = dayjs($(this).data('end'), 'h:mm:ss A');
 
-    if (now.isAfter(end)) {
+  function updateBlockColors() {
+    var CurrentTime = dayjs().hour();
+    $('.time-block').each(function(){
+
+    if (now.isAfter(currentTime)) {
       $(this).removeClass('present future').addClass('past');
-    } else if (now.isBetween(start, end, null, '[]')) {
-      $(this).removeClass('past future').addClass('present');
-    } else {
+    } else if (now.isBefore(currentTime)) {
       $(this).removeClass('past present').addClass('future');
+    } else {
+      $(this).removeClass('past future').addClass('present');
     }
-  });
+  })
 
   };
 
 
-  function highlightCurrentScheduleTime(now) {
-    $('.schedule-time').each(function(){
-      const start = dayjs($(this).data('start'), 'h:mm:ss A');
-      const end = dayjs($(this).data('end'), 'h:mm:ss A');
-  
-      if (now.isBetween(start, end)) {
-        $(this).addClass('current');
-      } else {
-        $(this).removeClass('current');
-      }
-    });
-  }
-
 });
-
-const saveBtns = $('savBtn');
-saveBtns.on('click', function(){
-  const hourId = $(this).closest('.time-block').attr('id');
-  const description = $(this).siblings('.description').val();
-  localStorage.setItem(hourId, description);
-});
-
-
-
-
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+ // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+$('.saveBtn').on('click', function () {
+
+var time = $(this).parent().attr("id");
+var text = $(this).siblings(".description").val();
+
+localStorage.setItem(time, text);
+console.log(text);
+console.log(time);
+
+});
+
+
+
+
+
+
+
+
+
+ 
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -73,6 +67,5 @@ saveBtns.on('click', function(){
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+ 
 // });
