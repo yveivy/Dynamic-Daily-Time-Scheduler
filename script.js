a5c2e46cad6221d6d19f57259f1b26d39fa77429
Valais Girl <Yevette@Hal-71.local>
2023-04-26 09:16:09 -0500

    updated time block color change function

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// the code isn't run until the browser has finished rendering all the elements in the html.
+
 $(document).ready(function() {
  //
   // TODO: Add code to display the current date in the header of the page.
- 
-  // Display current time in header
-  const currentTimeElement = $('#current-time');
-  setInterval(() => {
-    var now = dayjs();
-    const nowFormatted = now.format('dddd, MMMM D, YYYY hh:mm:ss A');
-    currentTimeElement.text(nowFormatted);
-  }, 1000);
 
-
-  function updateBlockColors() {
-    var CurrentTime = dayjs().hour();
-    $('.time-block').each(function(){
-
-    if (now.isAfter(currentTime)) {
-      $(this).removeClass('present future').addClass('past');
-    } else if (now.isBefore(currentTime)) {
-      $(this).removeClass('past present').addClass('future');
-    } else {
-      $(this).removeClass('past future').addClass('present');
-    }
-  })
-
+  var currentTimeElement = $('#current-time');
+  displayTime();
+  setInterval(displayTime, 1000);
+  
+  
+  function displayTime() {
+    var rightNow = dayjs().format('dddd, MMMM D, YYYY [at] hh:mm:ss A');
+    currentTimeElement.text(rightNow);
   };
 
+  var currentHour = dayjs().hour();
+  console.log(currentHour)
 
+// Hour 9 Block
+  var hourBlock9 = dayjs().hour(9);
+  var hourComponent = hourBlock9.hour();
+  console.log(hourBlock9)
+  var timeBlock9 = $(".time-block9");
+  
+
+  if (hourBlock9.isAfter(currentHour)) {
+    timeBlock9.removeClass('present future').addClass('past');
+  } else if (hourBlock9.isBefore(currentHour)) {
+    timeBlock9.removeClass('past present').addClass('future')
+  } else {
+    timeBlock9.removeClass('past future').addClass('present')};
+
+    
+  // Hour 10 Block
+  var hourBlock10 = dayjs().hour(10);
+  // console.log(hourBlock10)
+  var timeBlock10 = $(".time-block10");
+
+    if (hourBlock10.isAfter(currentHour)) {
+      timeBlock10.removeClass('present future').addClass('past');
+    } else if (hourBlock10.isBefore(currentHour)) {
+      timeBlock10.removeClass('past present').addClass('future')
+    } else {
+      timeBlock10.removeClass('past future').addClass('present')};
+
+      // console.log(hourBlock10)
 });
+
+
  // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -48,7 +66,31 @@ console.log(time);
 
 });
 
+// var counter = document.querySelector("#counter");
+// var addButton = document.querySelector("#add");
+// var subtractButton = document.querySelector("#subtract");
 
+// var count = localStorage.getItem("count");
+
+// counter.textContent = count;
+
+// addButton.addEventListener("click", function() {
+//   if (count < 24) {
+//     count++;
+//     counter.textContent = count;
+//     localStorage.setItem("count", count);
+//   }
+// });
+
+// subtractButton.addEventListener("click", function() {
+//   if (count > 0) {
+//     count--;
+//     counter.textContent = count;
+//     localStorage.setItem("count", count);
+
+    
+//   }
+// });
 
 
 
